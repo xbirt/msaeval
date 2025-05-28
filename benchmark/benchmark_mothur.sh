1c6a681547c1bbcc7a70a52d526919bb31a9d4bb
Cristi Dumitrescu <cristid@gmail.com>
2025-05-28 16:08:03 +0300

    Modified Mothur script to also benchmark against the full Silva alignment file

--- benchmark/benchmark_mothur.sh
+++ benchmark/benchmark_mothur.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Mothur args
+# processors - Number of CPU threads to use
+# search - Template search method - kmer (default) or suffix
+# ksize - Size of kmers for kmer search - 8 (default), range 5-12
+# align - Alignment algorithm - needleman (default) or gotoh
+# flip - Try reverse complement if alignment is poor - t (true, default) or f (false)
+# threshold - Cutoff for flipping sequence - 0.75 (default)
+
+
 THREADS=$(nproc)
 
 ./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=0.1k.fasta, reference=silva.seed_v138_2.align, processors=$THREADS)\""
@@ -22,10 +31,22 @@ gzip 1m.align
 ./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=2m.fasta, reference=silva.seed_v138_2.align, processors=$THREADS)\""
 gzip 2m.align
 
-# Mothur args
-# processors - Number of CPU threads to use
-# search - Template search method - kmer (default) or suffix
-# ksize - Size of kmers for kmer search - 8 (default), range 5-12
-# align - Alignment algorithm - needleman (default) or gotoh
-# flip - Try reverse complement if alignment is poor - t (true, default) or f (false)
-# threshold - Cutoff for flipping sequence - 0.75 (default)
+./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=0.1k.fasta, reference=silva.nr_v138_2.align, processors=$THREADS)\""
+gzip 0.1k.align
+./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=0.5k.fasta, reference=silva.nr_v138_2.align, processors=$THREADS)\""
+gzip 0.5k.align
+./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=1k.fasta, reference=silva.nr_v138_2.align, processors=$THREADS)\""
+gzip 1k.align
+./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=10k.fasta, reference=silva.nr_v138_2.align, processors=$THREADS)\""
+gzip 10k.align
+./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=50k.fasta, reference=silva.nr_v138_2.align, processors=$THREADS)\""
+gzip 50k.align
+./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=100k.fasta, reference=silva.nr_v138_2.align, processors=$THREADS)\""
+gzip 100k.align
+./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=500k.fasta, reference=silva.nr_v138_2.align, processors=$THREADS)\""
+gzip 500k.align
+./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=1m.fasta, reference=silva.nr_v138_2.align, processors=$THREADS)\""
+gzip 1m.align
+./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=2m.fasta, reference=silva.nr_v138_2.align, processors=$THREADS)\""
+gzip 2m.align
+
