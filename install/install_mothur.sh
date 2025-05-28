a4fc1b4b8a21db218ace64346cf48f0343ef1a22
Cristi Dumitrescu <cristid@gmail.com>
2025-05-28 12:57:19 +0300

    Updated Mothur install script to also download and unpack the reference databases

--- install/install_mothur.sh
+++ install/install_mothur.sh
@@ -8,3 +8,12 @@ sudo ln -s /lib/x86_64-linux-gnu/libreadline.so.8 /usr/lib/x86_64-linux-gnu/libr
 wget https://github.com/mothur/mothur/releases/download/v1.48.2/Mothur.linux_8.zip
 unzip Mothur.linux_8.zip
 rm Mothur.linux_8.zip
+
+# Fetch and extract the reference alignments.
+wget https://mothur.s3.us-east-2.amazonaws.com/wiki/silva.seed_v138_2.tgz
+tar xf silva.seed_v138_2.tgz
+rm silva.seed_v138_2.tgz
+
+wget https://mothur.s3.us-east-2.amazonaws.com/wiki/silva.nr_v138_2.tgz
+tar xf silva.nr_v138_2.tgz
+rm silva.nr_v138_2.tgz
