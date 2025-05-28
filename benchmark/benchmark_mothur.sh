00308f6e2415e6d667d0e49a58803ef744825338
Cristi Dumitrescu <cristid@gmail.com>
2025-05-28 12:57:49 +0300

    Updated Mothur benchmarking script to compress the resulting alignments due to size concerns (>80GB for 2m)

--- benchmark/benchmark_mothur.sh
+++ benchmark/benchmark_mothur.sh
@@ -3,16 +3,26 @@
 THREADS=$(nproc)
 
 ./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=0.1k.fasta, reference=silva.seed_v138_2.align, processors=$THREADS)\""
+# The alignment files need to be compressed to save space.
+gzip 0.1k.align
 ./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=0.5k.fasta, reference=silva.seed_v138_2.align, processors=$THREADS)\""
+gzip 0.5k.align
 ./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=1k.fasta, reference=silva.seed_v138_2.align, processors=$THREADS)\""
+gzip 1k.align
 ./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=10k.fasta, reference=silva.seed_v138_2.align, processors=$THREADS)\""
+gzip 10k.align
 ./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=50k.fasta, reference=silva.seed_v138_2.align, processors=$THREADS)\""
+gzip 50k.align
 ./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=100k.fasta, reference=silva.seed_v138_2.align, processors=$THREADS)\""
+gzip 100k.align
 ./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=500k.fasta, reference=silva.seed_v138_2.align, processors=$THREADS)\""
+gzip 500k.align
 ./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=1m.fasta, reference=silva.seed_v138_2.align, processors=$THREADS)\""
+gzip 1m.align
 ./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=2m.fasta, reference=silva.seed_v138_2.align, processors=$THREADS)\""
+gzip 2m.align
 
-# Args
+# Mothur args
 # processors - Number of CPU threads to use
 # search - Template search method - kmer (default) or suffix
 # ksize - Size of kmers for kmer search - 8 (default), range 5-12
