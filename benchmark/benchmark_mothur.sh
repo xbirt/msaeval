3263fa54f5a731ee18eb85ccae35cfe3ba5290e4
Cristi Dumitrescu <cristid@gmail.com>
2025-05-31 21:34:59 +0300

    Updated Mothur benchmarking script

--- benchmark/benchmark_mothur.sh
+++ benchmark/benchmark_mothur.sh
@@ -10,43 +10,151 @@
 
 
 THREADS=$(nproc)
-
-./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=0.1k.fasta, reference=silva.seed_v138_2.align, processors=$THREADS)\""
-# The alignment files need to be compressed to save space.
-gzip 0.1k.align
-./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=0.5k.fasta, reference=silva.seed_v138_2.align, processors=$THREADS)\""
-gzip 0.5k.align
+mkdir -p seed_db
 ./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=1k.fasta, reference=silva.seed_v138_2.align, processors=$THREADS)\""
+# The alignment files need to be compressed to save space.
 gzip 1k.align
+mv 1k.align.gz seed_db/
+mv mothur*.logfile seed_db/
+mv *.align_report seed_db/
 ./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=10k.fasta, reference=silva.seed_v138_2.align, processors=$THREADS)\""
 gzip 10k.align
+mv 10k.align.gz seed_db/
+mv mothur*.logfile seed_db/
+mv *.align_report seed_db/
 ./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=50k.fasta, reference=silva.seed_v138_2.align, processors=$THREADS)\""
 gzip 50k.align
+mv 50k.align.gz seed_db/
+mv mothur*.logfile seed_db/
+mv *.align_report seed_db/
 ./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=100k.fasta, reference=silva.seed_v138_2.align, processors=$THREADS)\""
 gzip 100k.align
+mv 100k.align.gz seed_db/
+mv mothur*.logfile seed_db/
+mv *.align_report seed_db/
 ./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=500k.fasta, reference=silva.seed_v138_2.align, processors=$THREADS)\""
 gzip 500k.align
+mv 500k.align.gz seed_db/
+mv mothur*.logfile seed_db/
+mv *.align_report seed_db/
 ./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=1m.fasta, reference=silva.seed_v138_2.align, processors=$THREADS)\""
 gzip 1m.align
+mv 1m.align.gz seed_db/
+mv mothur*.logfile seed_db/
+mv *.align_report seed_db/
 ./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=2m.fasta, reference=silva.seed_v138_2.align, processors=$THREADS)\""
 gzip 2m.align
+mv 2m.align.gz seed_db/
+mv mothur*.logfile seed_db/
+mv *.align_report seed_db/
 
-./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=0.1k.fasta, reference=silva.nr_v138_2.align, processors=$THREADS)\""
-gzip 0.1k.align
-./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=0.5k.fasta, reference=silva.nr_v138_2.align, processors=$THREADS)\""
-gzip 0.5k.align
+mkdir -p nr_db
 ./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=1k.fasta, reference=silva.nr_v138_2.align, processors=$THREADS)\""
 gzip 1k.align
+mv 1k.align.gz nr_db/
+mv mothur*.logfile nr_db/
+mv *.align_report nr_db/
 ./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=10k.fasta, reference=silva.nr_v138_2.align, processors=$THREADS)\""
 gzip 10k.align
+mv 10k.align.gz nr_db/
+mv mothur*.logfile nr_db/
+mv *.align_report nr_db/
 ./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=50k.fasta, reference=silva.nr_v138_2.align, processors=$THREADS)\""
 gzip 50k.align
+mv 50k.align.gz nr_db/
+mv mothur*.logfile nr_db/
+mv *.align_report nr_db/
 ./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=100k.fasta, reference=silva.nr_v138_2.align, processors=$THREADS)\""
 gzip 100k.align
+mv 100k.align.gz nr_db/
+mv mothur*.logfile nr_db/
+mv *.align_report nr_db/
 ./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=500k.fasta, reference=silva.nr_v138_2.align, processors=$THREADS)\""
 gzip 500k.align
+mv 500k.align.gz nr_db/
+mv mothur*.logfile nr_db/
+mv *.align_report nr_db/
 ./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=1m.fasta, reference=silva.nr_v138_2.align, processors=$THREADS)\""
 gzip 1m.align
+mv 1m.align.gz nr_db/
+mv mothur*.logfile nr_db/
+mv *.align_report nr_db/
 ./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=2m.fasta, reference=silva.nr_v138_2.align, processors=$THREADS)\""
 gzip 2m.align
+mv 2m.align.gz nr_db/
+mv mothur*.logfile nr_db/
+mv *.align_report nr_db/
 
+mkdir -p nr99_db
+./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=1k.fasta, reference=SILVA_138_SSURef_NR99_tax_silva_full_align_trunc.fasta, processors=$THREADS)\""
+gzip 1k.align
+mv 1k.align.gz nr99_db/
+mv mothur*.logfile nr99_db/
+mv *.align_report nr99_db/
+./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=10k.fasta, reference=SILVA_138_SSURef_NR99_tax_silva_full_align_trunc.fasta, processors=$THREADS)\""
+gzip 10k.align
+mv 10k.align.gz nr99_db/
+mv mothur*.logfile nr99_db/
+mv *.align_report nr99_db/
+./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=50k.fasta, reference=SILVA_138_SSURef_NR99_tax_silva_full_align_trunc.fasta, processors=$THREADS)\""
+gzip 50k.align
+mv 50k.align.gz nr99_db/
+mv mothur*.logfile nr99_db/
+mv *.align_report nr99_db/
+./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=100k.fasta, reference=SILVA_138_SSURef_NR99_tax_silva_full_align_trunc.fasta, processors=$THREADS)\""
+gzip 100k.align
+mv 100k.align.gz nr99_db/
+mv mothur*.logfile nr99_db/
+mv *.align_report nr99_db/
+./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=500k.fasta, reference=SILVA_138_SSURef_NR99_tax_silva_full_align_trunc.fasta, processors=$THREADS)\""
+gzip 500k.align
+mv 500k.align.gz nr99_db/
+mv mothur*.logfile nr99_db/
+mv *.align_report nr99_db/
+./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=1m.fasta, reference=SILVA_138_SSURef_NR99_tax_silva_full_align_trunc.fasta, processors=$THREADS)\""
+gzip 1m.align
+mv 1m.align.gz nr99_db/
+mv mothur*.logfile nr99_db/
+mv *.align_report nr99_db/
+./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=2m.fasta, reference=SILVA_138_SSURef_NR99_tax_silva_full_align_trunc.fasta, processors=$THREADS)\""
+gzip 2m.align
+mv 2m.align.gz nr99_db/
+mv mothur*.logfile nr99_db/
+mv *.align_report nr99_db/
+
+mkdir -p full_db
+./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=1k.fasta, reference=SILVA_138_SSURef_tax_silva_full_align_trunc.fasta, processors=$THREADS)\""
+gzip 1k.align
+mv 1k.align.gz full_db/
+mv mothur*.logfile full_db/
+mv *.align_report full_db/
+./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=10k.fasta, reference=SILVA_138_SSURef_tax_silva_full_align_trunc.fasta, processors=$THREADS)\""
+gzip 10k.align
+mv 10k.align.gz full_db/
+mv mothur*.logfile full_db/
+mv *.align_report full_db/
+./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=50k.fasta, reference=SILVA_138_SSURef_tax_silva_full_align_trunc.fasta, processors=$THREADS)\""
+gzip 50k.align
+mv 50k.align.gz full_db/
+mv mothur*.logfile full_db/
+mv *.align_report full_db/
+./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=100k.fasta, reference=SILVA_138_SSURef_tax_silva_full_align_trunc.fasta, processors=$THREADS)\""
+gzip 100k.align
+mv 100k.align.gz full_db/
+mv mothur*.logfile full_db/
+mv *.align_report full_db/
+./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=500k.fasta, reference=SILVA_138_SSURef_tax_silva_full_align_trunc.fasta, processors=$THREADS)\""
+gzip 500k.align
+mv 500k.align.gz full_db/
+mv mothur*.logfile full_db/
+mv *.align_report full_db/
+./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=1m.fasta, reference=SILVA_138_SSURef_tax_silva_full_align_trunc.fasta, processors=$THREADS)\""
+gzip 1m.align
+mv 1m.align.gz full_db/
+mv mothur*.logfile full_db/
+mv *.align_report full_db/
+./benchmark.sh "mothur/mothur" "\"#align.seqs(fasta=2m.fasta, reference=SILVA_138_SSURef_tax_silva_full_align_trunc.fasta, processors=$THREADS)\""
+gzip 2m.align
+mv 2m.align.gz full_db/
+mv mothur*.logfile full_db/
+mv *.align_report full_db/
